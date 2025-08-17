15103eba2646be8c577b140dec0074bd09a4bb8f
Martin Jílek <jilekm98@gmail.com>
2025-08-17 22:00:19 +0200

    Prepore for schedule management and refactor humidity handling

--- Config/FanControlSettings.cs
+++ Config/FanControlSettings.cs
@@ -5,4 +5,13 @@ public class FanControlSettings
     public double HumidityThreshold { get; set; }
     public int RelayGpioPin { get; set; }
     public int ReadIntervalMs { get; set; }
+    public Dictionary<string, List<ScheduleInterval>?>? Schedule { get; set; }
 }
+
+public class ScheduleInterval
+{
+    public TimeSpan StartTime { get; set; }
+    public TimeSpan EndTime { get; set; }
+    public bool Enabled { get; set; }
+}
+
--- Services/FanControlService.cs
+++ Services/FanControlService.cs
@@ -12,35 +12,102 @@ public class FanControlService : BackgroundService
     private readonly ISensorService _sensor;
     private readonly FanControlSettings _settings;
     private readonly IWeatherDataService _weatherDataService;
-    
+
     private bool _relayOn = false;
     private double _outsideHumidity = 0.0;
+    private DateTime _lastFanOn = DateTime.MinValue;
+    private double _hum;
+    private double _temp;
 
-    public FanControlService(RelayController relay, ISensorService sensor, IOptions<FanControlSettings> settings, IWeatherDataService weatherDataService)
+    private bool RelayOn
+    {
+        get => _relayOn;
+        set
+        {
+            if (value && !_relayOn)
+            {
+                _lastFanOn = DateTime.Now;
+            }
+
+            _relayOn = value;
+            _relay.SetRelay(_relayOn);
+        }
+    }
+
+
+
+
+    public FanControlService(RelayController relay, ISensorService sensor, IOptions<FanControlSettings> settings,
+        IWeatherDataService weatherDataService)
     {
         _relay = relay;
         _sensor = sensor;
         _settings = settings.Value;
         _weatherDataService = weatherDataService;
 
-        _weatherDataService.GetHumidityOnWeatherDataReceived += (data) =>
-        {
-            _outsideHumidity = data;
-        };
+        _weatherDataService.GetHumidityOnWeatherDataReceived += (data) => { _outsideHumidity = data; };
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            double temp = _sensor.ReadTemperature();
-            double hum = _sensor.ReadHumidity();
-            
-            _relayOn = hum > _settings.HumidityThreshold;
-            Console.WriteLine($"🌡️ {temp:F2}°C | 💧 {hum:F2}% | Relay: {(_relayOn ? "ON" : "OFF")} | Outside Humidity: {_outsideHumidity:F2}%");
-            _relay.SetRelay(_relayOn);
+            _temp = _sensor.ReadTemperature();
+            _hum = _sensor.ReadHumidity();
+
+            RelayOn = _hum > _settings.HumidityThreshold;
+
+            Console.WriteLine(
+                $"🌡️ {_temp:F2}°C | 💧 {_hum:F2}% | Relay: {(RelayOn ? "ON" : "OFF")} | Outside Humidity: {_outsideHumidity:F2}%");
 
             await Task.Delay(_settings.ReadIntervalMs, stoppingToken);
         }
     }
+
+    /// <summary>
+    /// Checks if current time is within the configured schedule.
+    /// 
+    /// If no schedule is set, run the default schedule
+    /// If no default is set then the schedule is 9:00 to 19:00
+    /// </summary>
+    /// <returns>bool</returns>
+    private bool CheckSchedule()
+    {
+        var now = DateTime.Now.TimeOfDay;
+        var reasonableDefault = new ScheduleInterval
+        {
+            StartTime = new TimeSpan(9, 0, 0),
+            EndTime = new TimeSpan(19, 0, 0)
+        };
+
+        try
+        {
+            var schedule = _settings?.Schedule;
+            if (schedule == null || schedule.Count == 0)
+            {
+                return now < reasonableDefault.StartTime && reasonableDefault.EndTime > now;
+            }
+
+            schedule.TryGetValue(DateTime.Now.DayOfWeek.ToString(), out var intervals);
+
+            if (intervals == null || intervals.Count == 0)
+            {
+                // empty intervals, get the default interval
+                intervals = schedule["default"];
+
+                if (intervals == null || intervals.Count == 0)
+                {
+                    return now < reasonableDefault.StartTime && reasonableDefault.EndTime > now;
+                }
+            }
+
+            return intervals.Any(i => i.StartTime >= now && i.EndTime <= now);
+
+        }
+        catch (Exception e)
+        {
+            return now < reasonableDefault.StartTime && reasonableDefault.EndTime > now;
+        }
+    }
+
 }
